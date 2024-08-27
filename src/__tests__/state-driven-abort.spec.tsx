06ba1b72e13d3f717936ef02e26016e69627d144
Yuchen Zhang <zhangyc@kanyun.com>
2024-08-27 17:22:17 +0800

    增加注释，解释 state-driven-abort.spec.tsx 的思路

--- src/__tests__/state-driven-abort.spec.tsx
+++ src/__tests__/state-driven-abort.spec.tsx
@@ -23,6 +23,16 @@ beforeEach(() => {
     expect(jest.getTimerCount()).toBe(0)
 })
 
+/**
+ * 在这个例子中，当用户点击 toggleRoom 按钮时，会在 connect/disconnect 方法中切换执行
+ * 进行 connect 会在 100ms 后设置连接状态是 conntected，所以如果用户连续点击 toggleRoom 按钮，那么状态的设置顺序是
+ * 
+ *         v-- connect()   v--- disconnect()
+ *   Idle ---> Connecting ---> Idle ------- 100ms --------> Connected
+ *              
+ * 这不是我们期望的，我们希望在调用 disconnect() 之后，能 abort 最后一次 connected 状态的设置
+ * 在下面的测试中，我们首先展示问题是如何出现的。首先从 setupRoomPage 开始
+ */
 test('不切换 route 情况下，异步任务得不到清理的例子', async () => {
     type AbortedFn = () => boolean;
     type AbortFn = () => void;
@@ -137,53 +147,64 @@ test('不切换 route 情况下，异步任务得不到清理的例子', async (
         </>
     }
 
+    /*
+      setupRoomPage 方法被 router loader 加载，在这个方法中，我们订阅了 viewState 的变化，来控制调用 connect / disconnect
+     */
     function setupRoomPage(abortContext: AbortContext): Promise<null> {
         abortContext.onAbort(viewStore.subscribe(state => {
+            // 如果 showRoom 则建立连接，否则断开连接
             if (state.showRoom) {
+                // 建立连接会理解设置状态为 Connecting，然后在 100ms 后设置状态为 Connected
                 void roomStore.getState().connect(abortContext, 100)
                 return;
             }
 
+            // 断开连接会立即设置状态为 Idle
             roomStore.getState().disconnect()
         }))
 
         return Promise.resolve(null);
     }
 
-    const abortContext = createAbortContext();
-
-    const router = createMemoryRouter([{
-        path: '/',
-        element: <RoomPage />,
-        loader: abortContext(setupRoomPage)
-    }])
-
-
-    render(<StrictMode>
-        <RouterProvider router={router} />
-    </StrictMode>)
-
     // Given: 初始页面
-    expect(await screen.findByText('RoomStatus: Idle')).toBeTruthy()
+    {
+        const abortContext = createAbortContext();
+        const router = createMemoryRouter([{
+            path: '/',
+            element: <RoomPage />,
+            loader: abortContext(setupRoomPage)
+        }])
 
-    // When: 点击 toggleRoom 按钮打开房间，开始进行连接
-    const toggleButton = await screen.findByText('toggleRoom')
-    act(() => {
-        toggleButton.click()
-    })
 
-    // Then: 房间状态变成连接中 / Connecting
-    expect(await screen.findByText('RoomStatus: Connecting')).toBeTruthy()
+        render(<StrictMode>
+            <RouterProvider router={router} />
+        </StrictMode>)
 
-    // When: 在 Connecting 状态下，点击按钮关闭 Room
-    act(() => {
-        toggleButton.click()
-    })
-    // Then: 此时房间状态应该是 Idle
-    expect(await screen.findByText('RoomStatus: Idle')).toBeTruthy()
+        expect(await screen.findByText('RoomStatus: Idle')).toBeTruthy()
+    }
 
-    // When: 清空 timer 队列
-    await actTimers()
+
+    // When: 连续两次点击 toggleRoom 按钮
+    {
+        // When: 点击 toggleRoom 按钮打开房间，开始进行连接
+        const toggleButton = await screen.findByText('toggleRoom')
+        act(() => {
+            toggleButton.click()
+        })
+
+        // Then: 房间状态变成连接中 / Connecting
+        expect(await screen.findByText('RoomStatus: Connecting')).toBeTruthy()
+
+        // When: 在 Connecting 状态下，点击按钮关闭 Room
+        act(() => {
+            toggleButton.click()
+        })
+        // Then: 此时房间状态应该是 Idle
+        expect(await screen.findByText('RoomStatus: Idle')).toBeTruthy()
+
+        // When: 清空 timer 队列，看页面最终状态
+        await actTimers()
+    }
 
     // Then: 可以发现 toggleRoom 后，前一个 timer 仍然执行了导致状态变为已连接
     expect(await screen.findByText('RoomStatus: Connected')).toBeTruthy()
