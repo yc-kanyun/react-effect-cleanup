bd7db7007242b78bb8d204e39d681c72ca45350e
e7h4n <perfectworks@gmail.com>
2024-09-04 17:39:20 +0800

    增加更多 toast 的例子

--- src/setup.tsx
+++ src/setup.tsx
@@ -11,6 +11,57 @@ export interface AppContext {
     routes: RouteObject[]
 }
 
+/**
+ * 在 Loading 状态时展示一个 toast，并在 loading 成功后 1000ms 后自动关闭
+ * 
+ * @param ctx 
+ * @param userStore 
+ */
+function setupLoadingToastWithPerfectCleanup(ctx: AbortContext, userStore: ReturnType<typeof createUserStore>) {
+    let loadingToastId: string | null = null;
+    ctx.onAbort(() => {
+        if (loadingToastId) {
+            toast.dismiss(loadingToastId)
+        }
+    })
+
+    ctx.onAbort(userStore.subscribe(state => {
+        if (state._loading && !loadingToastId) {
+            loadingToastId = toast.loading('Loading...')
+        }
+
+        if (!state._loading && loadingToastId) {
+            const currToastId = loadingToastId
+            loadingToastId = null
+
+            const timer = setTimeout(() => {
+                toast.dismiss(currToastId)
+            }, 1000)
+            ctx.onAbort(() => {
+                clearTimeout(timer)
+            })
+        }
+    }))
+}
+
+function setupLoadingToastWithoutCleanup(ctx: AbortContext, userStore: ReturnType<typeof createUserStore>) {
+    let loadingToastId: string | null = null;
+    ctx.onAbort(userStore.subscribe(state => {
+        if (state._loading && !loadingToastId) {
+            loadingToastId = toast.loading('Loading...')
+        }
+
+        if (!state._loading && loadingToastId) {
+            const currToastId = loadingToastId
+            setTimeout(() => {
+                toast.dismiss(currToastId)
+            }, 1000)
+
+            loadingToastId = null
+        }
+    }))
+}
+
 export function setupApp(): AppContext {
     const userStore = createUserStore()
     const rootAbortController = createAbortedController('root')
@@ -23,23 +74,8 @@ export function setupApp(): AppContext {
         // 下面的写法可以阻塞页面加载，让 router 展示全局 loading
         // await userStore.getState().fetch(ctx)
 
-        let loadingToastId: string | null = null;
-        ctx.onAbort(userStore.subscribe(state => {
-            if (state._loading && !loadingToastId) {
-                loadingToastId = toast.loading('Loading...')
-            }
-
-            if (!state._loading && loadingToastId) {
-                const currToastId = loadingToastId
-                const timer = setTimeout(() => {
-                    toast.dismiss(currToastId)
-                }, 1000)
-                ctx.onAbort(() => {
-                    clearTimeout(timer)
-                })
-                loadingToastId = null
-            }
-        }))
+        setupLoadingToastWithPerfectCleanup(ctx, userStore)
+        setupLoadingToastWithoutCleanup(ctx, userStore)
 
         return Promise.resolve(null)
     }
