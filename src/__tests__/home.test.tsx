8d4e261f7213ced0424ec66808262c3397342ffc
e7h4n <perfectworks@gmail.com>
2024-09-10 17:39:16 +0800

    enable 失败的测试

--- src/__tests__/home.test.tsx
+++ src/__tests__/home.test.tsx
@@ -2,13 +2,14 @@ import { setupApp } from "../setup";
 import { createMemoryRouter, RouterProvider } from "react-router-dom";
 import { StrictMode } from "react";
 import { act, cleanup, render, screen } from "@testing-library/react";
-import { describe, it, afterEach, beforeEach, expect, vi } from "vitest"
+import { describe, it, afterEach, beforeEach, expect, vitest } from "vitest"
 
 describe('Home 页面', () => {
     let appContext: ReturnType<typeof setupApp>;
     let router: ReturnType<typeof createMemoryRouter>;
 
     beforeEach(() => {
+        vitest.useFakeTimers()
         appContext = setupApp();
         router = createMemoryRouter(appContext.routes);
         render(<RouterProvider router={router} />, { wrapper: StrictMode })
@@ -16,17 +17,14 @@ describe('Home 页面', () => {
 
     afterEach(() => {
         cleanup()
+        vitest.useRealTimers()
     })
 
     it('应该能渲染出用户名', async () => {
         expect(await screen.findByText("Hi, The Octocat")).toBeTruthy()
     })
 
-    it.skip('切换页面到 /foo', async () => {
-        await vi.runAllTimersAsync()
-
-        expect(screen.getByText('Loading....')).toBeTruthy()
-
+    it('切换页面到 /foo', async () => {
         act(() => {
             void router.navigate('/foo')
         })
